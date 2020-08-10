a86b21e04c274cb5042fd579cdb28a933c98975b
Sam-Marx <sam-marx@protonmail.com>
2020-08-10 15:01:03 -0300

    added useful functions
    added removehttp function, to remove http/https from url
    added urltodomain function, to remove any slash and anything after it

--- lib/checksettings.py
+++ lib/checksettings.py
@@ -6,24 +6,35 @@ import os
 import functools
 import operator
 import requests
+import re
+
+from cmd2 import ansi
 
 class checkSettings:
 	'''This class is used to check settings, targets etc.'''
-	
+
 	def __init__(self):
 		pass
 
 	def checkHTTP(self, website: str) -> bool:
 		if website.startswith('http://') or website.startswith('https://'):
 			return True
+		return False
 
-	def checkReportsFolder(self):
+	def checkReportsFolder(self) -> bool:
 		if os.path.isdir('reports') is False:
 			return False
 		return True
 
-	def tupleToString(self, tuple_arg):
+	def tupleToString(self, tuple_arg: tuple) -> str:
 		return functools.reduce(operator.add, (tuple_arg))
+
+	def removeHTTP(self, website: str) -> str:
+		if self.checkHTTP(website):
+			return website.replace('http://', '').replace('https://', '')
+
+	def urltodomain(self, url: str) -> str:
+		return re.sub(r'(/[^\/]+$)', '', self.removeHTTP(url))
 
 	def checkUpdate(self):
 		try:
@@ -35,3 +46,7 @@ class checkSettings:
 			return None
 		except Exception as e:
 			return "Could not check Github's version."
+
+	def ansi_print(text: str):
+		'''Wraps style_aware_write so style can be stripped if needed'''
+		ansi.style_aware_write(sys.stdout, f'{text}\n\n')
